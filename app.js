75fadec26e5e180b7b4093ebfdf42376e88b1405
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-03-30 18:29:08 +0200

    router and controller first setup

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const app = express();
 
+const gamesRouter = require("./routers/gamesRouter");
+
 app.listen("http://localhost:3000", () => console.log(`Server listening on http://localhost:3000`));
