52441c3b747290ffe9253e7bc126282cceb2460f
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-03-31 17:39:58 +0200

    removed logic

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -3,15 +3,9 @@ const connection = require("../db/connection");
 
 // orderProductindex
 function orderProductindex(req, res) {
-  const orderProductsSQL = `SELECT * FROM products`; // temporaneo
-
-  connection.query(orderProductsSQL, (err, result) => {
-    if (err) return res.status(400);
-
-    res.status(200).json({
-      success: true,
-      result: result,
-    });
+  res.status(200).json({
+    success: true,
+    result: result,
   });
 }
 
