75e022a875fe6762c08c0b0d52e0ad9ce8564b97
leonardo-monti <monti.leonardo22@gmail.com>
2026-03-31 11:29:25 +0200

    setup db connection

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const express = require("express");
-const app = express();
-
+const connection = require("./db/connection");
 const gamesRouter = require("./routers/gamesRouter");
 
-app.listen("http://localhost:3000", () => console.log(`Server listening on http://localhost:3000`));
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/games", gamesRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server attivo");
+});
+
+app.get("/test-db", (req, res) => {
+  connection.query("SHOW TABLES", (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Errore DB",
+        details: err.message,
+      });
+    }
+
+    res.json({
+      message: "Connessione OK",
+      tables: results,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
--- routers/gamesRouter.js
+++ routers/gamesRouter.js
@@ -2,3 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const gameController = require("../controllers/gameController");
+
+router.get("/", (req, res) => {
+  res.json({ message: "games router ok" })
+})
+
+module.exports = router;
