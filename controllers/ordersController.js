94ce9319fc9cb0d6d005551c3212e0a8dea7b4ee
micheleferri01 <micheleferri095@gmail.com>
2026-04-01 01:28:06 +0200

    add: orders controller

---
+++ controllers/ordersController.js
@@ -0,0 +1,121 @@
+const connection = require('../db/connection');
+
+function index(req, res) { };
+
+function show(req, res) {
+    const { id } = req.params;
+    const sql = `
+    SELECT * 
+    FROM orders 
+    WHERE id = ?`;
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return errors(err);
+
+        const orderedProductsSql =
+            `SELECT * FROM orders_products  
+        INNER JOIN products 
+        ON orders_products.product_id = products.id
+        WHERE order_id = ?`
+
+        connection.query(orderedProductsSql, [id], (err, orderedProducts) => {
+            if (err) return errors(err);
+            return res.status(200).json({
+                success: true,
+                order: results,
+                productsOrdered: orderedProducts
+            })
+        })
+    })
+};
+
+function store(req, res) {
+    // dati che vengono recuperati dal body della richiesta
+    const {
+        name,
+        surname,
+        email,
+        shipping_address,
+        shipping_cap,
+        shipping_city,
+        shipping_country,
+        billing_address,
+        billing_cap,
+        billing_city,
+        billing_country,
+        sameAddress,
+        orderedProducts //é un array di oggetti che contiene tutti i prodotti che erano nel carrello
+    } = req.body;
+
+    // variabile per salvare l'id dell'ordine creato. servirà dopo per associare i prodotti nel carrello all'ordine.
+    let orderId;
+
+    const sql = `
+    INSERT INTO orders (name,
+        surname,
+        email,
+        shipping_address,
+        shipping_cap,
+        shipping_city,
+        shipping_country,
+        billing_address,
+        billing_cap,
+        billing_city,
+        billing_country)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?)
+    `;
+
+    const orderData = (sameAddress) => {
+        return sameAddress ? [
+            name,
+            surname,
+            email,
+            shipping_address,
+            shipping_cap,
+            shipping_city,
+            shipping_country,
+            shipping_address,
+            shipping_cap,
+            shipping_city,
+            shipping_country
+        ] : [
+            name,
+            surname,
+            email,
+            shipping_address,
+            shipping_cap,
+            shipping_city,
+            shipping_country,
+            billing_address,
+            billing_cap,
+            billing_city,
+            billing_country
+        ]
+    }
+
+    connection.query(sql,
+        orderData(sameAddress), (err, results) => {
+            if (err) return errors(err);
+            // ritorno l'id dell'ordine creato salvandolo direttamente nella variabile orderId appositamente creata.
+            return orderId = results.insertedId
+        });
+
+
+    // mappo gli orderedproducts così da salvarli nella apposita sezione del database
+    orderedProducts.map((orderedProduct) => {
+        const orderedGames = `
+        INSERT INTO products_orders (order_id, product_id, quantity, product_price, digital_copy_code) 
+        VALUES (?,?,?,?,?)`;
+
+        connection.query(orderedGames, [orderId, orderedProduct.id, orderedProduct.quantity, orderedProduct.price])
+    })
+
+}
+
+function errors(err) {
+    return res.status(500).json({ success: false, message: 'Errore interno del database operazione fallita' });
+};
+
+
+
+module.exports = { index, show, store }
