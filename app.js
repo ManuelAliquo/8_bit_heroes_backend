eb0447353357314a9991cf4c4a3b7f8d0da8fe61
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-04-01 10:22:34 +0200

    unified checkout and homepage logic

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
 const express = require("express");
-const connection = require("./db/connection");
-const gamesRouter = require("./routers/gamesRouter");
-const checkoutRouter = require("./routers/checkoutRouter");
-
 const app = express();
 const port = 3000;
 
+// routers
+const gamesRouter = require("./routers/gamesRouter");
+
+// middlewares
 app.use(express.json());
 
 app.use("/", gamesRouter);
-
-app.use("/", checkoutRouter);
-
-app.get("/", (req, res) => {
-  res.send("Server attivo");
-});
-
-app.get("/test-db", (req, res) => {
-  connection.query("SHOW TABLES", (err, results) => {
-    if (err) {
-      return res.status(500).json({
-        error: "Errore DB",
-        details: err.message,
-      });
-    }
-
-    res.json({
-      message: "Connessione OK",
-      tables: results,
-    });
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
--- routers/gamesRouter.js
+++ routers/gamesRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const homepageController = require("../controllers/homepageController");
-const gameController = require("../controllers/gameController");
 
-router.get("/", (req, res) => {
-  res.json({ message: "games router ok" });
-});
+const homepageController = require("../controllers/homepageController");
+const checkoutController = require("../controllers/checkoutController");
+
+/* CHECKOUT CONTROLLERS */
+router.get("/checkout", checkoutController.orderProductindex);
+router.patch("/checkout/order-update", checkoutController.updateOrderData);
 
 /* HOMEPAGE CONTROLLERS */
 router.get("/products/discounted", homepageController.discountedIndex);
