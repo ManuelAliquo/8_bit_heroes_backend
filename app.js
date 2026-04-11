237e673efee96ed7f99b5fe081fb54d9e7147a32
micheleferri01 <micheleferri095@gmail.com>
2026-04-11 11:19:32 +0200

    add: response in case of errors or endpoints not valid

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const gamesRouter = require("./routers/gamesRouter");
+const validEndpoint = require('./middlewares/validEndpoint');
+const errorsHandler = require('./middlewares/errorsHandler');
 const cors = require("cors");
 const app = express();
 const port = 3000;
@@ -10,6 +12,9 @@ app.use(express.static("public"))
 app.use(express.json());
 
 app.use("/", gamesRouter);
+
+app.use(validEndpoint);
+app.use(errorsHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
---
+++ middlewares/validEndpoint.js
@@ -0,0 +1,10 @@
+const validEndpoint = (req, res) => {
+    res.status(404).json(
+        {
+            errorCode: "404",
+            message: "Questo endpoint non esiste"
+        }
+    )
+}
+
+module.exports = validEndpoint;
